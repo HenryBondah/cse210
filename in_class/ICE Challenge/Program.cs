dcd8f0110b09ab69e1e0c3173ef043292d218410
HenryBondah <122046340+HenryBondah@users.noreply.github.com>
2023-06-05 12:31:15 -0600

    Update Program.cs

--- in_class/ICE Challenge/Program.cs
+++ in_class/ICE Challenge/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// ﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Elevator elevator1 = new Elevator();
+        Elevator elevator2 = new Elevator();
+        Elevator VIPelevator = new Elevator();
+
+        // Call the elevators and perform actions
+        elevator1.GoToFloor(30);
+        elevator1.OpenDoors();
+        elevator1.CloseDoors();
+        elevator1.callelevator();
+
+
+        elevator2.GoToFloor(30);
+        elevator2.OpenDoors();
+        elevator2.CloseDoors();
+        elevator2.callelevator();
+
+
+        VIPelevator.GoToFloor(31);
+        VIPelevator.OpenDoors();
+        VIPelevator.CloseDoors();
+        VIPelevator.callelevator();
+
+    }
+          public static void DisplayMenu()
+    {
+        Console.WriteLine("1. Elevator1");
+        Console.WriteLine("2. Elevator2");
+        Console.WriteLine("3. VIPelevator");
+        Console.WriteLine("0. Exit");
+        Console.Write("Enter your choice: ");
+    }
+
+}
